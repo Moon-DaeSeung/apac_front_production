f0f178b683a5688c8f9049d4a25734ba899ea27b
daeseungmoon <tmdeoans@snu.ac.kr>
2022-01-03 22:13:33 +0900

    불필요한 파일 삭제

--- src/utils/getURLParameters.ts
+++
@@ -1,7 +0,0 @@
-export const getURLParameters = () => {
-  const query = window.location.search
-  const urlParameters = new URLSearchParams(query)
-  const code = urlParameters.get('code')
-  const username = urlParameters.get('username')
-  return { code, username }
-}
